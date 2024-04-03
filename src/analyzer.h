3c090ac2a7e88e2d1b1ade4a52fe838c3806b19f
Filippo Olimpieri <olimpieri.1933529@studenti.uniroma1.it>
2024-04-03 13:36:06 +0000

    added spectrogram and scatterplot using python code

--- src/analyzer.h
+++ src/analyzer.h
@@ -16,8 +16,8 @@ public:
     Analyzer(){}
     std::vector<std::vector<double>> fast_fourier_transform(const std::string& input_file_path);
     std::vector<double> calculate_PSD(const std::string& input_file_path, double sampleRate);
-    void generate_IQ_Scatterplot(const std::string& input_file_path);
-    void generate_IQ_Spectrogram(const std::string& input_file_path, int overlap, int windowsize);
+    //void generate_IQ_Scatterplot(const std::string& input_file_path);
+    std::vector<std::vector<double>> generate_IQ_Spectrogram(const std::string& input_file_path, int overlap, int windowsize);
     std::vector<double> real_part_iq_sample(const std::string& input_file_path);
     std::vector<double> complex_part_iq_sample(const std::string& input_file_path);
     std::vector<std::vector<double>> get_iq_sample(const std::string& input_file_path);
