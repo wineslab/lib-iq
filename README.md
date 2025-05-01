0e7f1f1633baf61cefdd028bcc9020f90681b20e
Filippo Olimpieri <olimpieri.1933529@studenti.uniroma1.it>
2025-05-01 17:14:25 -0400

    Updated README and installation script
    Co-authored-by: Noemi Giustini <giustini.1933541@studenti.uniroma1.it>

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-# LIBIQ-010: A Library for I/Q Sample Analysis
-
-## Introduction
-`libiq` is library designed to facilitate the management and analysis of I/Q samples. It provides a suite of tools and functionalities that allow users to handle I/Q samples.
+# LibIQ Library
 
 ## Dependencies
 `libiq` is built on a foundation of several libraries:
@@ -14,76 +11,33 @@
 ## Building the Project
 Building `libiq` is a straightforward process. Here are the steps you need to follow:
 
-1. First be sure to build and install all the dependencies, in particular:
+Be sure to build and install all the dependencies, in particular:
     - **matio**: it can be build following the instructions on [matio's Github page](https://github.com/tbeu/matio?tab=readme-ov-file#22-building-matio)
     - **libsigmf**: it is an header only library but you need to build its dependencies following the instructions on [libsigmf's Github page](https://github.com/deepsig/libsigmf)
     - **SWIG**: it can be build following the instructions on [SWIG's Github page](https://github.com/swig/swig)
     - **FFTW**: it can be build following the instructions on [FFTW's Home Page](https://www.fftw.org/)
-3. Run `./remove_build.sh` to clean up the build directory. This removes all the files from the previous build, ensuring a fresh start.
-4. Execute `./build_auto.sh` to initiate the build process. This script compiles the source code and creates the necessary modules.
-5. Finally, run `./execute_prova.sh` to execute the `prova.py` script located in the `examples/prova.py` directory. This allows you to test the functionality.
 
-## Features
-`libiq` comes with a set of classes:
-
-- **Converter**: this class allows you to convert the I/Q sample's file in different format. 
-    - `from_bin_to_mat`: This function allows you to convert a `.bin` file containing an I/Q sample into a `.mat` file. This is particularly useful when you need to analyze I/Q samples using tools that support `.mat` files.
-    - `from_mat_to_sigmf`: This function enables you to convert a `.mat` file into a `.sgmf` file. This is beneficial when you want to store signal metadata along with the signal data in a standardized format.
-- **Analyzer**: this class provides a set of functions for analyzing I/Q samples.
-    - `fast_fourier_transform`: This function takes the path to an I/Q sample file as input and returns the Fast Fourier Transform (FFT) of the I/Q samples. The FFT is a mathematical technique that transforms a function of time into a function of frequency.
-    - `calculate_PSD`: This function calculates the Power Spectral Density (PSD) of the I/Q samples. The PSD is a measure of the power intensity in the frequency domain. The function takes the path to an I/Q sample file and the sample rate as inputs.
-    - `generate_IQ_Scatterplot`: This function takes the path to an I/Q sample file as input and generates a scatter plot of the I/Q samples. The scatter plot is a graphical representation of the variation of a signal.
-    - `generate_IQ_Spectrogram`: This function takes in input the path to an I/Q sample file, the overlap and the window size and generates a spectrogram of the I/Q samples. A spectrogram is a visual representation of the spectrum of frequencies in a signal as it varies with time. The colors in the spectrogram represent the intensity of the signal at that particular frequency and at that particular moment in time. Warmer colors (like red) indicate a high signal intensity, while cooler colors (like blue) indicate a low signal intensity.
-    - `get_iq_sample`: This function reads the file at the given `input_file_path` and returns the IQ sample represented as a complex vector.
-    - `complex_part_iq_sample`: This function reads the file at the given `input_file_path` and returns the complex part of the IQ data sample.
-    - `real_part_iq_sample`: This function reads the file at the given `input_file_path` and returns the real part of the IQ data sample.
-
-
-
-## Examples
+A script that performs the installation of all the dependencies is given toghether with the library, to execute is you need to run 
 ```
-import libiq
-converter = libiq.Converter() 
+./libiq_installer.sh
+```
+This script will update all submodules, build and install all the necessary libraries, creates a python virtual environment and download all python dependencies inside this virtual environment.
 
-#insert metadata values according to Sigmf standard
-converter.freq_lower_edge = 213456
-converter.freq_upper_edge = 3456768
-converter.sample_rate = 23456
-converter.frequency = 567890
-converter.global_index = 9999
-converter.sample_start = 1
-converter.hw = "hello"
-converter.version = "1.0.0"
-
-#input_file_path1 is the path to .bin or .iq file
-#output_file_path1 is the path to .mat file
-converter.from_bin_to_mat(input_file_path1, output_file_path1)
-
-#input_file_path2 is the path to .mat file
-#output_file_path2 is the path to .sigmf-meta file
-converter.from_mat_to_sigmf(input_file_path2, output_file_path2)
+To activate the virtual environment run:
+```
+source .libiqvenv310/bin/activate
 ```
 
+Once this is done, to build LibIQ, you need to run:
 ```
-import libiq
-import src_python.scatterplot as scplt
-import src_python.spectrogram as sp
-
-#input_file_path is the path to .bin or .iq file
-analyzer = libiq.Analyzer()
-iq = analyzer.get_iq_sample(input_file_path)
-complex_iq = analyzer.complex_part_iq_sample(input_file_path)
-real_iq = analyzer.real_part_iq_sample(input_file_path)
-
-fft_result = analyzer.fast_fourier_transform(input_file_path)
-
-sample_rate = 1000;
-psd_result = analyzer.calculate_PSD(input_file_path, sample_rate)
-
-iq = analyzer.get_iq_sample(input_file_path)
-scplt.scatterplot(iq)
-
-psd = analyzer.generate_IQ_Spectrogram(input_file_path, 0, 256)
-sample_rate = 20 * 5
-sp.spectrogram(psd, sample_rate)
+hatch env create
 ```
+Once this is done, run:
+```
+hatch build
+```
+Once this is done, run:
+```
+pip install dist/libiq-0.1.0.tar.gz
+```
+
--- libiq_installer.sh
+++ libiq_installer.sh
@@ -2,8 +2,37 @@
 clear
 BASE_PATH="$(pwd)"
 
-BASE_DIR="$BASE_PATH/libiq"
-LIBS_DIR="$BASE_DIR/libs"
+if [[ "$(basename "$BASE_PATH")" == "libiq" ]]; then
+    BASE_DIR="$BASE_PATH"
+    LIBS_DIR="$BASE_DIR/libs"
+    
+    echo "Already inside libiq. Skipping clone."
+    
+    echo "Switching to 'libiq_clean' branch..."
+    git switch libiq_clean || { echo "Failed to switch to libiq_clean branch."; exit 1; }
+    git pull origin libiq_clean || { echo "Failed to pull updates from libiq_clean branch."; exit 1; }
+
+    echo "Updating submodules..."
+    git submodule update --init --recursive libs/libsigmf libs/RFDataFactory libs/sdr_channelizer libs/zlib || echo "Problem initializing submodules"
+    git submodule update --init libs/matio
+else
+    BASE_DIR="$BASE_PATH/libiq"
+    LIBS_DIR="$BASE_DIR/libs"
+
+    if [ -d "$BASE_DIR" ]; then
+        echo "The directory $BASE_DIR already exists. Skipping git clone and submodules."
+    else
+        echo "Cloning libiq repository..."
+        git clone --branch libiq_clean https://github.com/wineslab/libiq.git "$BASE_DIR" || { echo "Error cloning libiq."; exit 1; }
+
+        echo "Entering $BASE_DIR/"
+        cd "$BASE_DIR" || { echo "Directory $BASE_DIR not found!"; exit 1; }
+
+        echo "Updating submodules in $BASE_DIR"
+        git submodule update --init --recursive libs/libsigmf libs/RFDataFactory libs/sdr_channelizer libs/zlib || echo "Problem initializing submodules"
+        git submodule update --init libs/matio
+    fi
+fi
 
 ZLIB_DIR="$LIBS_DIR/zlib"
 HDF5_ARCHIVE="$LIBS_DIR/hdf5-1_14_3.tar.gz"
@@ -38,49 +67,35 @@ check_command() {
         else
             version=$(eval "$2")
         fi
-        echo "$1 è già installato ($version). Salto l'installazione."
+        echo "$1 is already installed ($version). Skipping installation."
     else
         echo "Installing $1..."
-        sudo apt install -y "$3" || { echo "Errore durante l'installazione di $1."; exit 1; }
+        sudo apt install -y "$3" || { echo "Error installing $1."; exit 1; }
     fi
 }
 
 check_python_package() {
     if "$PYTHON_BIN" -m pip show "$1" &> /dev/null; then
-        echo "Il pacchetto '$1' è già installato."
+        echo "Package '$1' is already installed."
     else
-        echo "Installazione del pacchetto '$1'..."
-        "$PYTHON_BIN" -m pip install "$1" || { echo "Errore durante l'installazione di '$1'."; exit 1; }
+        echo "Installing package '$1'..."
+        "$PYTHON_BIN" -m pip install "$1" || { echo "Error installing package '$1'."; exit 1; }
     fi
 }
 
 install_package() {
     if dpkg -l | grep -qw "$1"; then
-        echo "$1 è già installato."
+        echo "$1 is already installed."
     else
-        echo "Installazione di $1..."
-        sudo apt install -y "$1" || { echo "Errore durante l'installazione di $1."; exit 1; }
+        echo "Installing $1..."
+        sudo apt install -y "$1" || { echo "Error installing $1."; exit 1; }
     fi
 }
 
-if [ -d "$BASE_DIR" ]; then
-    echo "La directory $BASE_DIR esiste già. Salto git clone e l'inizializzazione dei submodules."
-else
-    echo "Clonazione del repository libiq..."
-    git clone --branch libiq_clean https://github.com/wineslab/libiq.git "$BASE_DIR" || { echo "Errore durante il clonaggio di libiq."; exit 1; }
-
-    echo "Entering $BASE_DIR/"
-    cd "$BASE_DIR" || { echo "Directory $BASE_DIR non trovata!"; exit 1; }
-
-    echo "Updating submodules di libiq in $BASE_DIR"
-    git submodule update --init --recursive libs/libsigmf libs/RFDataFactory libs/sdr_channelizer libs/zlib || echo "Problema nell'inizializzazione dei submodules"
-    git submodule update --init libs/matio
-fi
-
 if command -v "$PYTHON_BIN" &> /dev/null; then
-    echo "Python $PYTHON_VERSION è già installato ($(python3.10 --version))."
+    echo "Python $PYTHON_VERSION is already installed ($($PYTHON_BIN --version))."
 else
-    echo "Python $PYTHON_VERSION non trovato. Installazione in corso..."
+    echo "Python $PYTHON_VERSION not found. Installing..."
     sudo add-apt-repository ppa:deadsnakes/ppa -y
     sudo apt update
     install_package "python3.10"
@@ -92,22 +107,23 @@ fi
 
 install_package "graphviz"
 
-cd "$BASE_DIR" || { echo "Directory $BASE_DIR non trovata!"; exit 1; }
+cd "$BASE_DIR" || { echo "Directory $BASE_DIR not found!"; exit 1; }
 
 if [ -d "$VENV_DIR" ]; then
-    echo "L'ambiente virtuale $VENV_DIR esiste già. Lo attivo."
+    echo "The virtual environment $VENV_DIR already exists. Activating it."
 else
-    echo "Creazione dell'ambiente virtuale $VENV_DIR con Python $PYTHON_VERSION..."
-    "$PYTHON_BIN" -m venv "$VENV_DIR" || { echo "Errore durante la creazione dell'ambiente virtuale."; exit 1; }
+    echo "Creating virtual environment $VENV_DIR with Python $PYTHON_VERSION..."
+    "$PYTHON_BIN" -m venv "$VENV_DIR" || { echo "Error while creating the virtual environment."; exit 1; }
 fi
 
-echo "Attivazione dell'ambiente virtuale $VENV_DIR..."
-source "$VENV_DIR/bin/activate" || { echo "Errore durante l'attivazione dell'ambiente virtuale."; exit 1; }
+echo "Activating virtual environment $VENV_DIR..."
+source "$VENV_DIR/bin/activate" || { echo "Error while activating the virtual environment."; exit 1; }
 
-echo "Aggiornamento di pip..."
-pip install --upgrade pip || { echo "Errore durante l'aggiornamento di pip."; exit 1; }
+echo "Upgrading pip..."
+pip install --upgrade pip || { echo "Error while upgrading pip."; exit 1; }
 
 check_python_package "matplotlib"
+check_python_package "hatch"
 
 check_command "$SWIG_BIN" "swig -version | grep 'SWIG Version' | awk '{print $3}'" "swig"
 
@@ -117,48 +133,50 @@ check_command "$GPP_BIN" "g++ --version | head -n 1 | awk '{print $4}'" "g++"
 
 if command -v "$LIBTOOL_BIN" &> /dev/null; then
     version=$("$LIBTOOL_BIN" --version | head -n 1 | awk '{print $4}')
-    echo "$LIBTOOL_BIN è già installato ($version). Salto l'installazione."
+    echo "$LIBTOOL_BIN is already installed (version $version). Skipping installation."
 else
     echo "Installing libtool and libtool-bin..."
-    sudo apt install -y libtool libtool-bin || { echo "Errore durante l'installazione di libtool."; exit 1; }
+    sudo apt install -y libtool libtool-bin || { echo "Error installing libtool."; exit 1; }
 fi
 
 if [ -f "$ZLIB_INCLUDE" ]; then
-    echo "zlib è già presente in /usr/local/include/. Salto la compilazione e installazione."
+    echo "zlib is already present in /usr/local/include/. Skipping build and installation."
 else
     echo "Entering $ZLIB_DIR/"
-    cd "$ZLIB_DIR" || { echo "Directory $ZLIB_DIR non trovata!"; exit 1; }
+    cd "$ZLIB_DIR" || { echo "Directory $ZLIB_DIR not found!"; exit 1; }
 
     echo "Building zlib..."
-    cmake . || { echo "Errore durante la configurazione di CMake."; exit 1; }
-    cmake --build . --parallel "$NPROC" || { echo "Errore durante la compilazione di zlib."; exit 1; }
+    cmake . || { echo "Error during CMake configuration."; exit 1; }
+    cmake --build . --parallel "$NPROC" || { echo "Error during zlib compilation."; exit 1; }
 
-    echo "Installing zlib in /usr/local/include/"
-    sudo cmake --install . || { echo "Errore durante l'installazione di zlib."; exit 1; }
+    echo "Installing zlib to /usr/local/include/"
+    sudo cmake --install . || { echo "Error during zlib installation."; exit 1; }
 
-    echo "Installation of zlib completed successfully!"
+    echo "zlib installation completed successfully!"
 fi
 
 if [ -d "$HDF5_INCLUDE" ]; then
-    echo "HDF5 è già installato in /usr/local/include/. Salto la configurazione, compilazione e installazione."
+    echo "HDF5 is already installed in /usr/local/include/. Skipping configuration, build, and installation."
 else
     if [ -d "$HDF5_DIR" ]; then
-        echo "La directory $HDF5_DIR è già presente. Salto il download e lo spacchettamento."
+        echo "The directory $HDF5_DIR already exists. Skipping download and extraction."
     else
         if [ -f "$HDF5_ARCHIVE" ]; then
-            echo "Il file $HDF5_ARCHIVE è già presente. Salto il download."
+            echo "The file $HDF5_ARCHIVE already exists. Skipping download."
         else
             echo "Downloading HDF5..."
-            wget -O "$HDF5_ARCHIVE" https://github.com/HDFGroup/hdf5/archive/refs/tags/hdf5-1_14_3.tar.gz || { echo "Errore durante il download di HDF5."; exit 1; }
+            wget -O "$HDF5_ARCHIVE" https://github.com/HDFGroup/hdf5/archive/refs/tags/hdf5-1_14_3.tar.gz \
+                || { echo "Error downloading HDF5."; exit 1; }
         fi
 
         echo "Unpacking $HDF5_ARCHIVE"
-        tar -xzf "$HDF5_ARCHIVE" -C "$LIBS_DIR" || { echo "Errore durante lo spacchettamento di HDF5."; exit 1; }
-        rm $HDF5_ARCHIVE
+        tar -xzf "$HDF5_ARCHIVE" -C "$LIBS_DIR" \
+            || { echo "Error unpacking HDF5."; exit 1; }
+        rm "$HDF5_ARCHIVE"
     fi
 
     echo "Entering $HDF5_DIR/"
-    cd "$HDF5_DIR" || { echo "Directory $HDF5_DIR non trovata!"; exit 1; }
+    cd "$HDF5_DIR" || { echo "Directory $HDF5_DIR not found!"; exit 1; }
 
     echo "Starting HDF5 configuration..."
     ./configure --prefix=/usr/local/include/hdf5 --enable-cxx
@@ -168,92 +186,86 @@ else
 fi
 
 if [ -f "$MATIO_INCLUDE" ] || [ -f "$MATIO_PUBCONF_INCLUDE" ]; then
-    echo "matio è già installato in /usr/local/include/. Salto la configurazione, compilazione e installazione."
+    echo "matio is already installed in /usr/local/include/. Skipping configuration, build, and installation."
 else
     echo "Entering $MATIO_DIR/"
-    cd "$MATIO_DIR" || { echo "Directory $MATIO_DIR non trovata!"; exit 1; }
+    cd "$MATIO_DIR" || { echo "Directory $MATIO_DIR not found!"; exit 1; }
 
-    echo "Eseguendo autogen.sh..."
-    ./autogen.sh || { echo "Errore durante l'esecuzione di autogen.sh."; exit 1; }
+    echo "Running autogen.sh..."
+    ./autogen.sh || { echo "Error running autogen.sh."; exit 1; }
     
-    echo "Configurazione di matio..."
-    ./configure --enable-mat73=yes --with-default-file-ver=7.3 --with-hdf5="$HDF5_INCLUDE" || { echo "Errore durante la configurazione di matio."; exit 1; }
+    echo "Configuring matio..."
+    ./configure --enable-mat73=yes --with-default-file-ver=7.3 --with-hdf5="$HDF5_INCLUDE" \
+        || { echo "Error configuring matio."; exit 1; }
 
-    echo "Compilazione di matio..."
-    make -j"$NPROC" || { echo "Errore durante la compilazione di matio."; exit 1; }
+    echo "Building matio..."
+    make -j"$NPROC" || { echo "Error building matio."; exit 1; }
 
-    echo "Installazione di matio..."
-    sudo make install PREFIX=/usr/local/include/matio || { echo "Errore durante l'installazione di matio."; exit 1; }
+    echo "Installing matio..."
+    sudo make install PREFIX=/usr/local/include/matio \
+        || { echo "Error installing matio."; exit 1; }
 
     echo "matio installed successfully!"
 fi
 
 if [ -d "$SIGMF_INCLUDE" ]; then
-    echo "libsigmf è già installato in /usr/local/include/. Salto la compilazione e installazione."
+    echo "libsigmf is already installed in /usr/local/include/. Skipping build and installation."
 else
     echo "Entering $LIBSIGMF_DIR/"
-    cd "$LIBSIGMF_DIR" || { echo "Directory $LIBSIGMF_DIR non trovata!"; exit 1; }
+    cd "$LIBSIGMF_DIR" || { echo "Directory $LIBSIGMF_DIR not found!"; exit 1; }
 
     if [ ! -d "build" ]; then
         echo "Creating build directory..."
-        mkdir build || { echo "Errore durante la creazione della directory build."; exit 1; }
+        mkdir build || { echo "Error creating build directory."; exit 1; }
     else
-        echo "La directory build esiste già. Utilizzo la directory esistente."
+        echo "Build directory already exists. Using existing directory."
     fi
 
-    cd build || { echo "Directory build non trovata!"; exit 1; }
+    cd build || { echo "Build directory not found!"; exit 1; }
 
     echo "Configuring libsigmf with CMake..."
-    cmake ../ || { echo "Errore durante la configurazione con CMake."; exit 1; }
+    cmake ../ || { echo "Error configuring with CMake."; exit 1; }
 
     echo "Building libsigmf..."
-    make -j"$NPROC" || { echo "Errore durante la compilazione di libsigmf."; exit 1; }
+    make -j"$NPROC" || { echo "Error building libsigmf."; exit 1; }
 
     echo "Installing libsigmf..."
-    sudo make install || { echo "Errore durante l'installazione di libsigmf."; exit 1; }
+    sudo make install || { echo "Error installing libsigmf."; exit 1; }
 
     echo "libsigmf installed successfully!"
 fi
 
 if [ -f "$FFTW_INCLUDE" ]; then
-    echo "FFTW è già installato in /usr/local/include/. Salto la configurazione, compilazione e installazione."
+    echo "FFTW is already installed in /usr/local/include/. Skipping configuration, build, and installation."
 else
     if [ -d "$FFTW_DIR" ]; then
-        echo "La directory $FFTW_DIR è già presente. Salto il download e lo spacchettamento."
+        echo "Directory $FFTW_DIR already exists. Skipping download and extraction."
     else
         if [ -f "$FFTW_ARCHIVE" ]; then
-            echo "Il file $FFTW_ARCHIVE è già presente. Salto il download."
+            echo "File $FFTW_ARCHIVE already exists. Skipping download."
         else
             echo "Downloading FFTW..."
-            wget -O "$FFTW_ARCHIVE" https://fftw.org/fftw-3.3.10.tar.gz || { echo "Errore durante il download di FFTW."; exit 1; }
+            wget -O "$FFTW_ARCHIVE" https://fftw.org/fftw-3.3.10.tar.gz || { echo "Error downloading FFTW."; exit 1; }
         fi
 
-        echo "Unpacking $FFTW_ARCHIVE"
-        tar -xzf "$FFTW_ARCHIVE" -C "$LIBS_DIR" || { echo "Errore durante lo spacchettamento di FFTW."; exit 1; }
+        echo "Extracting $FFTW_ARCHIVE"
+        tar -xzf "$FFTW_ARCHIVE" -C "$LIBS_DIR" || { echo "Error extracting FFTW."; exit 1; }
         rm "$FFTW_ARCHIVE"
     fi
 
     echo "Entering $FFTW_DIR/"
-    cd "$FFTW_DIR" || { echo "Directory $FFTW_DIR non trovata!"; exit 1; }
+    cd "$FFTW_DIR" || { echo "Directory $FFTW_DIR not found!"; exit 1; }
 
-    echo "Starting FFTW configuration..."
-    ./configure --enable-shared --with-pic --enable-threads || { echo "Errore durante la configurazione di FFTW."; exit 1; }
+    echo "Configuring FFTW..."
+    ./configure --enable-shared --with-pic --enable-threads || { echo "Error configuring FFTW."; exit 1; }
 
     echo "Building FFTW..."
-    make -j"$NPROC" || { echo "Errore durante la compilazione di FFTW."; exit 1; }
+    make -j"$NPROC" || { echo "Error building FFTW."; exit 1; }
 
     echo "Installing FFTW..."
-    sudo make install || { echo "Errore durante l'installazione di FFTW."; exit 1; }
+    sudo make install || { echo "Error installing FFTW."; exit 1; }
 
     echo "FFTW installed successfully!"
-fi
-
-if command -v gnuradio-config-info &> /dev/null; then
-    version=$(gnuradio-config-info --version)
-    echo "GNURadio è già installato (Versione: $version). Salto l'installazione."
-else
-    echo "Installing GNURadio..."
-    sudo apt install gnuradio -y || { echo "Errore durante l'installazione di GNURadio."; exit 1; }
 fi
 
 sudo ldconfig
